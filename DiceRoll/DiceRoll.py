a3dfd6a726eb7b75a9d7d18a4fafc259a63d5728
rhodso <rhodsoprivate@gmail.com>
2018-07-09 12:48:36 +0100

    Added code
    Made the code (hopefully) work

--- DiceRoll/DiceRoll.py
+++ DiceRoll/DiceRoll.py
@@ -1,1 +1,35 @@
+#Imports
+import discord
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+import platform
+import datetime
 
+#Def log method
+def log(Message):
+    print(str(datetime.datetime.now())+ '   ' + str(Message))
+
+#Def Method to read token from the file
+def getToken():
+    tokenFile = open('token.txt','r')
+    token = tokenFile.read()
+    return token
+
+#Define client
+client = Bot(description='', command_prefix=';', pm_help = False)
+
+#Startup sequence
+@client.event
+async def on_ready():
+    log('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
+    log('')
+    log('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+    return await client.change_presence(game=discord.Game(name='with dice')) 
+
+#Commands
+@client.command()
+async def ping(*args):
+
+	await client.say('Pong!')
+client.run(str(getToken()))
