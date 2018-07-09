b1dd369f52b70a6f1a456d7d3c8f4380c8d98ad5
rhodso <rhodsoprivate@gmail.com>
2018-07-09 17:06:14 +0100

    Command handling WIP
    Need to finish this bit off. Then commands will be fine

--- DiceRoll/DiceRoll.py
+++ DiceRoll/DiceRoll.py
@@ -6,6 +6,9 @@ from discord.ext import commands
 import platform
 import datetime
 
+#Set prefix character
+prefix = "|"
+
 #Def log method
 def log(Message):
     print(str(datetime.datetime.now())+ '   ' + str(Message))
@@ -17,19 +20,38 @@ def getToken():
     return token
 
 #Define client
-client = Bot(description='', command_prefix=';', pm_help = False)
+client = Bot(description='', command_prefix=prefix, pm_help = False)
 
 #Startup sequence
 @client.event
 async def on_ready():
+    log('Initialising...')
     log('Logged in as '+client.user.name+' (ID:'+client.user.id+') | Connected to '+str(len(client.servers))+' servers | Connected to '+str(len(set(client.get_all_members())))+' users')
     log('')
     log('Current Discord.py Version: {} | Current Python Version: {}'.format(discord.__version__, platform.python_version()))
+    log('Ready!')
     return await client.change_presence(game=discord.Game(name='with dice')) 
 
-#Commands
-@client.command()
-async def ping(*args):
 
-	await client.say('Pong!')
+##Commands
+#@client.command()
+#async def ping(*args):
+#    log("Runing ping command...")
+#    await client.say('Pong!')
+
+#async def rtd(*args):
+#    log("Running rtd command...")
+#    await client.say("WIP")
+
+@client.event
+async def on_message(message):
+    if(message.content[:1] == prefix):
+        
+        if(message.content == (prefix + "ping")):
+            log("Running ping command...")
+            await client.send_message(client.get_channel(), 'Pong!')
+
+    else:
+        pass
+
 client.run(str(getToken()))
