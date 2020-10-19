bd24995296f7a0a649c976fc7da4f7acdfb9f20c
rhodso <rhodsoprivate@gmail.com>
2020-10-19 16:21:09 +0100

    Updated commands for 1.5 It's not 0.15 it's 1.5 I'm dumb

--- DiceRoll/DiceRoll.py
+++ DiceRoll/DiceRoll.py
@@ -45,19 +45,19 @@ async def on_message(message):
         #   log("Running [Command name] command...")
         #   Do stuff
         #   End with:
-        #   await client.send_message(client.get_channel(message.channel.id), [Message (result of command)])
+        #   await message.channel.send( [Message (result of command)])
 
         #Help command
         if(message.content == (prefix + "help")):
             log("Running help command...")
-            await client.send_message(client.get_channel(message.channel.id),'Usage: |rtd [NumberOfDice] [SidesOfDice]')
-            await client.send_message(client.get_channel(message.channel.id),'[NumberOfDice] = The number of dice to roll. If more than 1, will add all scores together')
-            await client.send_message(client.get_channel(message.channel.id),'[SidesOfDice] = The number of sides the dice has. Must be more than 1')
+            await message.channel.send('Usage: |rtd [NumberOfDice] [SidesOfDice]')
+            await message.channel.send('[NumberOfDice] = The number of dice to roll. If more than 1, will add all scores together')
+            await message.channel.send('[SidesOfDice] = The number of sides the dice has. Must be more than 1')
 
         #Ping command
         if(message.content == (prefix + "ping")):
             log("Running ping command...")
-            await client.send_message(client.get_channel(message.channel.id), 'Pong!')
+            await message.channel.send( 'Pong!')
 
         #RollTheDice Command
         if(message.content[:4] == (prefix + "rtd")):
@@ -80,7 +80,7 @@ async def on_message(message):
 
                 #If there's only 1 dice, no need to add up and give total
                 if(NoOfDice == 1):
-                    await client.send_message(client.get_channel(message.channel.id), 'You rolled a ' + str(random.randint(1,SidesOfDice)) + '!')
+                    await message.channel.send( 'You rolled a ' + str(random.randint(1,SidesOfDice)) + '!')
                 else:
                     total = 0
                     #Add the dice up and give total
@@ -88,18 +88,18 @@ async def on_message(message):
                     for i in range (0,NoOfDice):
                         #Roll the dice and tell the user what they rolled
                         roll = random.randint(1,SidesOfDice-1)
-                        await client.send_message(client.get_channel(message.channel.id), 'You rolled a ' + str(roll) + '!')
+                        await message.channel.send( 'You rolled a ' + str(roll) + '!')
                         #Add roll to total
                         total = total + roll
                     
                         #Output total
-                    await client.send_message(client.get_channel(message.channel.id), 'Your total is ' + str(total) + '!')      
+                    await message.channel.send( 'Your total is ' + str(total) + '!')      
             
             except:
                 #Catch exception
                 log("Something went wrong in rtd command. Parameters probably not typed correctly")
                 #Tell the user to check their parameters 
-                await client.send_message(client.get_channel(message.channel.id), 'Something went wrong, did you type the parameters correctly?')
+                await message.channel.send( 'Something went wrong, did you type the parameters correctly?')
 
     else:
         #Message is not a command, ignore
